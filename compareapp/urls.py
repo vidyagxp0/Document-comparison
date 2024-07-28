1c149041960d3bcf69436ecb2a07484b41beea0c
Aditya Patel <adityapatel8912@gmail.com>
2024-07-29 04:04:21 +0530

    Some changes

--- compareapp/urls.py
+++ compareapp/urls.py
@@ -4,5 +4,8 @@ from compareapp import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('form/', views.form, name="form"),
+    path('bulk-pdf/', views.bulkPDF, name="bulk-pdf"),
+    path('bulk-doc/', views.bulkDoc, name="bulk-doc"),
 ]
